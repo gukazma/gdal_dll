30d47049d11c4026cd0ddea8ea6df15b58249843
gukazma <gukazma@proton.me>
2024-05-15 18:12:42 +0800

    🎉 init: project init

---
+++ Modules/GDAL_DLL/GDALDLL.cpp
@@ -0,0 +1,35 @@
+#include "GDALDLL.h"
+#include  <gdal.h>
+#include <gdal_priv.h>
+#include <iostream>
+void GDAL_DLL() {
+    // 注册所有的GDAL驱动
+    GDALAllRegister();
+    CPLGetConfigOption("GDAL_DATA", NULL);
+    // 打开栅格数据集
+    GDALDataset* poDataset = (GDALDataset*)GDALOpen("your_raster_dataset.tif", GA_ReadOnly);
+    if (poDataset == NULL) {
+        printf("Error: Unable to open the raster dataset.\n");
+        return ;
+    }
+
+    // 获取数据集的地理变换信息
+    double adfGeoTransform[6];
+    poDataset->GetGeoTransform(adfGeoTransform);
+
+    // 获取数据集的投影信息
+    const char* pszProjection = poDataset->GetProjectionRef();
+
+    // 输出地理变换信息
+    printf("GeoTransform:\n");
+    for (int i = 0; i < 6; i++) {
+        printf("%f ", adfGeoTransform[i]);
+    }
+    printf("\n");
+
+    // 输出投影信息
+    printf("Projection: %s\n", pszProjection);
+
+    // 释放数据集
+    GDALClose(poDataset);
+}
--- Tests/TEST.cpp
+++ Tests/TEST.cpp
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
+#include "GDALDLL.h"
 TEST(MYTEST0, A)
 {
-
+    GDAL_DLL();
 }
